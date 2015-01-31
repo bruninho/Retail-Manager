db27aa2120cce0c04b2dbb4170b370e794d00e2e
Bruno Walraven <brunowalraven@gmail.com>
2015-01-31 20:35:51 +0000

    user login complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user,   only: [:edit, :update]    
+  before_action :correct_user,   only: [:edit, :update] 
+  before_action :admin_user,     only: :destroy
     
   def new
     @user = User.new
@@ -40,7 +41,12 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
+  
+    def destroy 
+        User.find(params[:id]).destroy
+        flash[:success] = "User deleted"
+        redirect_to root_path        
+    end
 
     # strong parameters method to avoid SQL injection
     private
@@ -61,6 +67,10 @@ class UsersController < ApplicationController
     def correct_user 
         @user = User.find(params[:id])
         redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user 
+        redirect_to(root_url) unless current_user.admin?
     end
 
 end
